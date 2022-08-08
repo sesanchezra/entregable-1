9eb1b16bfbf4195a0caa66029596c7e0fa78b1ae
Sebastian Sanchez <sesanchezra@unal.edu.co>
2022-08-07 21:28:04 -0500

    Search box CSS 65% - funtionality 0%

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import SearchCategory from './SearchCategory'
-import { GiReceiveMoney, GiMeditation } from "react-icons/gi";
+import { GiReceiveMoney, GiMeditation ,GiInspiration } from "react-icons/gi";
 import { AiFillBook } from "react-icons/ai";
 import { IoIosRocket } from "react-icons/io";
+
+
 
 const icon = [
     {
         icon: <GiReceiveMoney />,
-        tittle: 'Abundance'
+        tittle: 'Abundance',
+        color: '#fcc2c2'
     },
     {
         icon: <GiMeditation />,
-        tittle: 'Spirituality'
+        tittle: 'Spirituality',
+        color: '#54ddea'
     },
     {
         icon: <AiFillBook />,
-        tittle: 'Learn'
+        tittle: 'Learn',
+        color: '#a77eff'
     },
     {
         icon: <IoIosRocket />,
-        tittle: 'Inspire'
+        tittle: 'Growth',
+        color: '#81b6ff'
+    },
+    {
+        icon: <GiInspiration />,
+        tittle: 'Inspire',
+        color: '#efb204'
     },
     
     
@@ -28,7 +39,9 @@ const icon = [
 const Search = () => {
     return (
         <div className='search'>
+            <h2>Categories</h2>
             <div className='search-box'>
+                
                 {
                     icon.map(icon => (
                         <SearchCategory
--- src/components/SearchCategory.jsx
+++ src/components/SearchCategory.jsx
@@ -5,9 +5,9 @@ import { GiReceiveMoney } from 'react-icons/gi'
 const SearchCategory = ({icon}) => {
     return (
         <div className='search-category'>
-            <button className='button-icon'>
+            <button className='button-icon' style={{color: `${icon?.color}`}}>
                 {icon?.icon}
-                <h4 className='button-tittle'>{icon?.tittle}</h4>
+                <h4 className='button-tittle' style={{color: '#808080'}}>{icon?.tittle}</h4>
             </button>
         </div>
     )
